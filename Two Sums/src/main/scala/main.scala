cf2bbf0c9de2271827efe9e67fa959307b2fbbed
Encapsulateed <alexey.mitroshkin@gmail.com>
2024-07-31 18:20:32 +0300

    two sums

---
+++ Two Sums/src/main/scala/main.scala
@@ -0,0 +1,28 @@
+@main
+def main(): Unit = {
+  import Solution._
+ // println(twoSum(Array(2, 7, 11, 15), 9).mkString("Array(", ", ", ")"))
+  assert(twoSum(Array(2, 7, 11, 15), 9) sameElements Array(0, 1))
+}
+
+object Solution {
+
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    // map<int,int> : nums(i) => index
+    var map = Map[Int, Int]()
+    var out = Array.emptyIntArray
+
+    for (i <- nums.indices) {
+      val diff = target - nums(i) // == nums(j)
+
+      if map.keys.exists(_ == diff) then {
+        out = Array(map(diff), i)
+      }
+      else {
+        map += (nums(i) -> i)
+      }
+    }
+    out
+  }
+
+}
